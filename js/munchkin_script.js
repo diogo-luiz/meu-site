435426352cf23a11f1c3b607beb2aded3583741c
Diogo Luiz <profissional.diogoribeiro@gmail.com>
2024-03-08 20:28:42 -0300

    Adicionando modal

--- js/munchkin_script.js
+++ js/munchkin_script.js
@@ -106,3 +106,18 @@ document.getElementById('increaseAllied').onclick = function() {
     document.getElementById('countAllied').innerHTML = countAllied;
 
 }
+
+
+// Modal
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"))
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
